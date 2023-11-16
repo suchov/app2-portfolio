1fbf3b8f07a5cd218075bca3324859c15b056eae
Artem Sychov <[artem.suchov@gmail.com]>
2023-11-16 14:59:21 +0100

    add discription on the new collumns

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=600)
+    st.image("images/photo.png")
 
 with col2:
     st.title("Artem Sychov")
     content = """
-    Result-oriented Engineering Manager with 19+ years of experience. Excel in building offshore and onsite teams, deep practical knowledge in leadership and motivation. Extensive experience in R&D, Product Development, Engineering, QC, and a full delivery cycle.
+    Result-oriented Engineering Manager with 19+ years of experience.
+    Excel in building offshore and onsite teams, deep practical knowledge in
+    leadership and motivation. Extensive experience in R&D,
+    Product Development, Engineering, QC, and a full delivery cycle.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
